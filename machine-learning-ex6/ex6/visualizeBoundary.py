b8cfe21024ca9aa61def952e00576c25ecd8076b
dream <Dream.Chang@testritegroup.com>
2019-03-21 11:38:29 +0800

    Update homework-6 code

--- machine-learning-ex6/ex6/visualizeBoundary.py
+++ machine-learning-ex6/ex6/visualizeBoundary.py
@@ -9,4 +9,4 @@ def visualize_boundary(clf, X, x_min, x_max, y_min, y_max):
 
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
-    plt.contour(xx, yy, Z, levels=[0], colors='r')
+    plt.contour(xx, yy, Z, levels=[0], colors='b')
